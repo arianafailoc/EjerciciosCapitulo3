a49e4e862a21a50a9737c62ae9c70af1c45bb2cb
arianafailoc <ariana.failoc@ucsp.edu.pe>
2022-03-29 02:57:48 -0500

    cree la clase Date

---
+++ Date.h
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+
+class Date {
+    public:
+        Date(int _month,int _day, int _year){
+            setMonth(_month);
+            day = _day;
+            year = _year;
+        }
+
+        void displaysDate () {
+            std::cout << day<<"/"<< month<<"/"<<year;
+
+        }
+//SET     
+         void setMonth(int _month) {
+            month = _month;
+            if (_month >= 1 && _month <= 12) {
+                 month = _month;
+            }
+            else { 
+                month = 1;
+            }
+        }
+         void setDay(int _day) {
+            day = _day;
+            }
+         void setYear(int _year) {
+            year = _year;
+            }
+
+//GET      
+       int getMonth() const {
+            return month;
+        }
+
+        int getDay() const {
+            return day;
+        }
+
+        int getYear() const {
+            return year;
+        }
+
+
+    private:
+        int month;
+        int day;
+        int year;
+
+};
